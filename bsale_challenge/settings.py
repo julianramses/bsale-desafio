8fb3e07f2c4b36642b35bb0008c269b46a825a31
Julian Escobar <julianramses@MBP-de-Julian.lan>
2022-06-15 02:20:48 -0400

    cors para frontend

--- bsale_challenge/settings.py
+++ bsale_challenge/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'productos',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -135,3 +137,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = 'staticfiles'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
